58e24b0519e5ac4d4253fe1ab6f0f01732f99328
XEviLSpaWnX <d.howley77@gmail.com>
2021-01-14 18:41:44 +0000

    Initail Commit

---
+++ src/main/java/com/xevilspawnx/crucialgadgets/CrucialGadgets.java
@@ -0,0 +1,27 @@
+package com.xevilspawnx.crucialgadgets;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("crucialgadgets")
+public class CrucialGadgets {
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public CrucialGadgets() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
