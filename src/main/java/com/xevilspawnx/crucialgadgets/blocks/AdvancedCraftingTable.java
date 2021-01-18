954a2ae6940e7bf9278ab774c4ef26ce0f5c8a3e
XEviLSpaWnX <d.howley77@gmail.com>
2021-01-18 04:17:08 +0000

    Correct texture for crafting table showing

--- src/main/java/com/xevilspawnx/crucialgadgets/blocks/AdvancedCraftingTable.java
+++ src/main/java/com/xevilspawnx/crucialgadgets/blocks/AdvancedCraftingTable.java
@@ -1,12 +1,17 @@
 package com.xevilspawnx.crucialgadgets.blocks;
 
-import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.CraftingTableBlock;
-import net.minecraft.block.SoundType;
+import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
 import net.minecraftforge.common.ToolType;
 
 public class AdvancedCraftingTable extends CraftingTableBlock {
+
+    private static final DirectionProperty FACING = HorizontalBlock.HORIZONTAL_FACING;
 
     public AdvancedCraftingTable() {
         super(AbstractBlock.Properties.create(Material.WOOD)
@@ -16,5 +21,23 @@ public class AdvancedCraftingTable extends CraftingTableBlock {
                 .harvestLevel(0)
                 .setRequiresTool()
         );
+    }
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, Rotation rot) {
+        return state.with(FACING, rot.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, Mirror mirrorIn) {
+        return state.rotate(mirrorIn.toRotation(state.get(FACING)));
+    }
+
+    @Override
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
     }
 }
